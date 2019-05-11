9df218b30b07e4c9a2e8d87cf97acb7af5016991
Sunny Chang Mei <mkaleader@gmail.com>
2019-05-11 16:37:40 -0400

    complete analytics board

--- client/components/Analytics.js
+++ client/components/Analytics.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
-import './Analytics.css'
-export const Analytics = () => (
+import './Analytics.css';
+
+export const DisconnectedAnalytics = ({
+  minuteExercised,
+  favoriteExercise,
+  percentCompleted
+}) => (
   <div id="analytics">
     <div className="box">
       <h2 className="box-header">Analytics</h2>
@@ -10,19 +16,77 @@ export const Analytics = () => (
           <tbody>
             <tr className="analytics-table-row">
               <td className="analytics-name">Total Minutes Exercised:</td>
-              <td id="total-exercised">95</td>
+              <td id="total-exercised">{minuteExercised}</td>
             </tr>
             <tr className="analytics-table-row">
               <td className="analytics-name">Favorite Exercise:</td>
-              <td>Running</td>
+              <td>{favoriteExercise}</td>
             </tr>
             <tr className="analytics-table-row">
               <td className="analytics-name">Percentage Completed:</td>
-              <td id="percentage-completed">75%</td>
+              <td id="percentage-completed">{percentCompleted}</td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
   </div>
-)
+);
+
+const collectExercisesFromWorkouts = workouts => {
+  return workouts.reduce(
+    (exercises, workout) => [...exercises, ...workout.exercises],
+    []
+  );
+};
+
+const countMinutesSpentExercising = exercises => {
+  return exercises.reduce((minutes, exercise) => {
+    if (exercise.completed) return minutes + exercise.duration;
+    return minutes;
+  }, 0);
+};
+
+const calculateFavoriteExercise = exercises => {
+  const countObj = {};
+  let maxCount = 0;
+  let currentVictor = null;
+  
+  exercises.forEach(exercise => {
+    countObj[exercise.name] = countObj[exercise.name]
+      ? countObj[exercise.name] + 1
+      : 1;
+
+    if (countObj[exercise.name] > maxCount) {
+      maxCount = countObj[exercise.name];
+      currentVictor = exercise.name;
+    }
+  });
+  return currentVictor;
+};
+
+const calculatePercentCompleted = exercises => {
+  if (exercises.length) {
+    return Math.round(
+      (100 * exercises.filter(exercise => exercise.completed).length) /
+        exercises.length,
+      2
+    );
+  }
+  return 100;
+};
+
+const mapStateToProps = state => {
+  const exercises = collectExercisesFromWorkouts(state.workouts);
+
+  return {
+    minuteExercised: countMinutesSpentExercising(exercises),
+    favoriteExercise: calculateFavoriteExercise(exercises),
+    percentCompleted: calculatePercentCompleted(exercises)
+  };
+};
+
+export const Analytics = connect(
+  mapStateToProps,
+  null
+)(DisconnectedAnalytics);
