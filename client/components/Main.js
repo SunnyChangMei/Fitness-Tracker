0c991bb2dacfe34e7f990f38b7bb7c8aafef8c1b
Sunny Chang Mei <mkaleader@gmail.com>
2019-05-11 13:07:11 -0400

    add mapDispatchToProps

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { Analytics } from './Analytics'
-import { ListWorkouts } from './ListWorkouts'
+import { buildFetchWorkoutsThunk } from '../store';
 
-import './Main.css'
-export const Main = (props) => {
-  const { workouts } = props
+import { Analytics } from './Analytics';
+import { ListWorkouts } from './ListWorkouts';
 
-  return (
-    <div id="main">
-      <Analytics />
-      <ListWorkouts workouts={ workouts } />
-    </div>
-  )
+import './Main.css';
+
+export class DisconnectedMain extends React.Component {
+  componentDidMount() {
+    this.props.fetchWorkouts();
+  }
+  // const { workouts } = props;
+
+  render() {
+    return (
+      <div id="main">
+        <Analytics />
+        <ListWorkouts />
+      </div>
+    );
+  }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchWorkouts: () => dispatch(buildFetchWorkoutsThunk())
+  };
+};
+
+export const Main = connect(
+  null,
+  mapDispatchToProps
+)(DisconnectedMain);
